4790a2a9b3323d58a1fdfd55f4cf6ac9475b7c34
geraldkachi <fitzgeraldkachi@gmail.com>
2022-10-18 09:08:43 +0100

    using vue for the first time building a vue dashboard

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+   <div class="flex w-full min-h-screen font-sans bg-gray-100">
+    <SideBar />
+    <main class="flex flex-col flex-1 gap-3 p-4">
+      <header>
+        <h1 class="text-3xl font-semibold leading-loose text-gray-700">POS System</h1>
+      </header>
+      <hr class="border-red-700">
+      <Card />
+      <Table />
+    </main>
+    <aside class="flex flex-col gap-y-6 pt-6 pr-6 w-96">
+      <RightSide />
+
+    </aside>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SideBar from './components/SideBar.vue'
+
+import RightSide from './components/RightSide.vue'
+import Table from './components/Table.vue';
+import Card from './components/Card.vue';
+
+</script>
+
